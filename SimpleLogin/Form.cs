9436db3c61a569e22b90d342abfe7cbe102c3fd5
ken <113131558+kenjinx0@users.noreply.github.com>
2025-03-26 10:56:14 +0530

    Form.cs

--- SimpleLogin/Form.cs
+++ SimpleLogin/Form.cs
@@ -1,1 +1,46 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
+namespace SimpleLoginApp
+{
+    public partial class LoginForm : Form
+    {
+        // Define the username and password
+        private string predefinedUsername = "user123";
+        private string predefinedPassword = "pass123";
+
+        public LoginForm()
+        {
+            InitializeComponent();0
+        }
+
+        // Handle the Submit button click event
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string enteredUsername = txtUsername.Text;
+            string enteredPassword = txtPassword.Text;
+
+            if (enteredUsername == predefinedUsername && enteredPassword == predefinedPassword)
+            {
+                // If the login is successful, show the WelcomeForm
+                WelcomeForm welcomeForm = new WelcomeForm();
+                welcomeForm.Show();
+                this.Hide();  // Hide the login form
+            }
+            else
+            {
+                // If the login fails, show a warning message
+                lblMessage.Text = "Incorrect username or password!";
+                lblMessage.ForeColor = Color.Red;
+            }
+        }
+
+        // Form Load Event to set some initial properties
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            lblMessage.ForeColor = Color.Black;
+        }
+    }
+}
